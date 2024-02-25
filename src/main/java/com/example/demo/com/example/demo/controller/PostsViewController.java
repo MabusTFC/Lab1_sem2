96afaa336436f6ebc8d0e3b23fad564f9d86b4d0
MabusTFC <113986000+MabusTFC@users.noreply.github.com>
2024-02-25 15:42:37 +0500

    3.1 make class controller

--- src/main/java/com/example/demo/com/example/demo/controller/PostsViewController.java
+++ src/main/java/com/example/demo/com/example/demo/controller/PostsViewController.java
@@ -6,19 +6,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
 
 @Controller
 public class PostsViewController {
-    @ResponseBody
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String list() {
-        return "Здесь будет главная страница";
+    public String list(Model model) {
+        model.addAttribute("appName", "Моё супер приложение");
+        return "list";
     }
-
 
     @ResponseBody
     @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
     public String single(@PathVariable("id") Long id) {
         return "Здесь будет страница поста №" + id;
     }
+
+
+
 }
