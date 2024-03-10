e72047bcbfd9002c393359e38d31e684ffeb379b
MabusTFC <113986000+MabusTFC@users.noreply.github.com>
2024-03-10 17:50:23 +0500

    bootstar 6.2

--- src/main/java/com/example/demo/om/example/demo/model/Post.java
+++ src/main/java/com/example/demo/om/example/demo/model/Post.java
@@ -1,12 +1,20 @@
 package com.example.demo.om.example.demo.model;
+import java.util.Date;
 import java.util.Scanner;
 public class Post {
     private String text;
     private Integer likes;
 
+    private Date creationDate;
 
-    public Post(String text){
+
+
+    public Date getCreationDate(){return creationDate;}
+
+
+    public Post(String text, Date creationDate){
         this.text = text;
+        this.creationDate = creationDate;
     }
 
     public String getText(){
--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -1,9 +1,12 @@
 package com.example.demo.service;
 
 import com.example.demo.om.example.demo.model.Post;
+import com.fasterxml.jackson.databind.ser.std.StdArraySerializers;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -11,9 +14,9 @@ public class PostService {
     //List<Post> posts;
     public List<Post> listAllPosts(){
 
-         Post post1 = new Post("Бубубубуубуб");
-         Post post2 = new Post("ФФЫФЫФЫФЫФЫФ");
-         Post post3 = new Post("УРАУРАУРАУРА");
+         Post post1 = new Post("Бубубубуубуб", new Date(124, Calendar.DECEMBER,11));
+         Post post2 = new Post("ФФЫФЫФЫФЫФЫФ", new Date(104,Calendar.FEBRUARY,25));
+         Post post3 = new Post("УРАУРАУРАУРА", new Date(123,Calendar.SEPTEMBER,14));
          List<Post> posts = new ArrayList<Post>();
          posts.add(post1);
          posts.add(post2);
